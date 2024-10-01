b66db573b8a520058b10d671d45d92272feb8d53
ferdi <ferdinandusdevon23@gmail.com>
2024-10-01 15:36:05 +0700

    anu

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -2,8 +2,8 @@
   <div class="sidenav-header">
     <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
     <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="{{ route('dashboard') }}">
-        <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="...">
-        <span class="ms-3 font-weight-bold">App Reminder Billing</span>
+        <img src="../assets/img/logo.png" class="navbar-brand-img h-100" alt="...">
+        <span class="ms-3 font-weight-bold">Reminder Billing</span>
     </a>
   </div>
   <hr class="horizontal dark mt-0">
